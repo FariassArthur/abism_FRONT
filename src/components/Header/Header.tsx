064035e73aa86cdc77677326aaed6dcb1097e9f8
arthurfarias <arthurfarias@arthurfarias.fariasarthur>
2024-04-25 14:35:48 -0300

    adding components and signin page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,24 @@
+//css
+import "./Header.module.scss";
+
 //router
 import { Link } from "react-router-dom";
 
 //icons
-import { FaUserPlus, FaSun, FaMoon } from "react-icons/fa";
+import {
+  FaUserPlus,
+  FaUserCog,
+  FaUserTimes,
+  FaSun,
+  FaMoon,
+} from "react-icons/fa";
 import { GrLanguage } from "react-icons/gr";
+import { IoLogOutSharp } from "react-icons/io5";
 
 interface Props {
-  config?: boolean,
-  auth?: boolean,
-  noAuth?: boolean
+  config?: boolean;
+  auth?: boolean;
+  logout?: boolean;
 }
 
 const Header = (props: Props) => {
@@ -22,7 +32,8 @@ const Header = (props: Props) => {
 
       <section>
         <div className="icon">
-          <FaUserPlus />
+          {props.auth && <FaUserPlus />}
+          {props.config && <FaUserCog />}
         </div>
 
         <div className="icon">
@@ -34,6 +45,12 @@ const Header = (props: Props) => {
         <div className="icon">
           <GrLanguage />
         </div>
+
+        {props.logout && (
+          <div className="icon">
+            <IoLogOutSharp />
+          </div>
+        )}
       </section>
     </div>
   );
