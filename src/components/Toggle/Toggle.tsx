dc6cf9b5ea125ae38dfb1d4d6bafb6a87212afa0
arthurfarias <arthurfarias@arthurfarias.fariasarthur>
2024-04-30 10:56:08 -0300

    adding more css

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 //css
 import styles from "./Toggle.module.scss";
 
 //icon
 import { FaArrowsAltH } from "react-icons/fa";
 
+//components
+import Header from "../Header/Header";
+
 const Toggle = () => {
+  const [click, setClick] = useState<boolean>(false);
+
   return (
-    <div id={styles.toggle}>
-      <FaArrowsAltH />
+    <div onClick={() => click ? setClick(false) : setClick(true)} id={styles.toggle}>
+
+      {click && <Header toggle={true} />}
+      
+      <div className={styles.iconToggle}>
+        <FaArrowsAltH />
+      </div>
+      
     </div>
   );
 };
