3a61cffbbbd035e2d325ca9aebc0b759ac2b2766
arthurfarias <arthurfarias@arthurfarias.fariasarthur>
2024-06-19 18:42:05 -0300

    Adding new component

---
+++ src/pages/CreatePoem/CreatePoem.tsx
@@ -0,0 +1,13 @@
+import styles from "./CreatePoem.module.scss"
+
+type Props = {}
+
+const CreatePoem = (props: Props) => {
+  return (
+    <div className={styles.containner}>
+
+    </div>
+  )
+}
+
+export default CreatePoem
