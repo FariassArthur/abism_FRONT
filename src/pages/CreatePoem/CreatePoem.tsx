8525a5f750aa7b405355d3e35aac92bb4c88fdf6
arthurfarias <arthurfarias@arthurfarias.fariasarthur>
2024-06-19 19:08:39 -0300

    fix

--- src/pages/CreatePoem/CreatePoem.tsx
+++ src/pages/CreatePoem/CreatePoem.tsx
@@ -1,11 +1,20 @@
 import styles from "./CreatePoem.module.scss"
 
+// image
+import image from "../../assets/images/headerimage.png";
+
+//component
+import BodyHeader from "../../components/BodyHeader/BodyHeader";
+
 type Props = {}
 
 const CreatePoem = (props: Props) => {
   return (
-    <div className={styles.containner}>
-
+    <div id={styles.createPoem}>
+          <img src={image} alt="" />
+          <div className={styles.containner}>
+              <BodyHeader searchAssets={false} />
+          </div>
     </div>
   )
 }
